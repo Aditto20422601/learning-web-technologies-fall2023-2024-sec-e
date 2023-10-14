4f51454ad8e8aaf377a981d04323312a225086bf
Aditto20422601 <adittosarkerasp@gmail.com>
2023-10-15 01:24:53 +0600

    add css

--- LAB 4.1/BloodGroup.php
+++ LAB 4.1/BloodGroup.php
@@ -1,15 +1,22 @@
 <html>
+    <style>
+        fieldset {
+            width: 150px; /* Adjust the width as needed */
+            height: auto; /* Adjust the height as needed */
+        }
+    </style>
     <fieldset>
-    <legend>Blood Group</legend>
-    <from method="post" action="testbloodgroup.php">
-    <select name="">
-        <option value="A+">A+</option>         
-        <option value="B+">B+</option>         
-        <option value="AB+">AB+</option>         
-        <option value="AB-">AB-</option> 
-        <option value="O+">O+</option>  
-        <option value="O-">O-</option>          
-    </select> <br>
+        <legend>Blood Group</legend>
+        <form method="post" action="testbloodgroup.php">
+            <select name="">
+                <option value="A+">A+</option>         
+                <option value="B+">B+</option>         
+                <option value="AB+">AB+</option>         
+                <option value="AB-">AB-</option> 
+                <option value="O+">O+</option>  
+                <option value="O-">O-</option>          
+            </select> <br>
+        </form>
     </fieldset>
     <br><input type="submit" name="" value="submit"/><br>
 </html>
